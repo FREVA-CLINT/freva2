60170707c8fcefeea1734862ba7b9f47d534324e
Brian Lewis <keller@csh.rit.edu>
2022-12-01 15:47:21 +0100

    Remove history

--- history/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse, HttpRequest
-from django.contrib.auth.decorators import login_required
-from rest_framework.decorators import api_view
-
-
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "history/index.html", {"title": "History main page"})
