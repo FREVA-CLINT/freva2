e8e251470960ab8b82111e4a5aa58da10ee366b6
Brian Lewis <keller@csh.rit.edu>
2023-01-09 17:19:38 +0100

    Add ability to pass engine params to toil and try to enable docker in docker for testing (#7)
    * Add ability to pass engine params to toil and try to enable docker in
    docker for testing
    
    * Fix isort issues
    
    * Fix broken status call on unfinished workflows
    
    * Change workdir to /tmp for docker to work
    
    * Have toil use /tmp/toil-workdir instead of /tmp
    
    * Change from host /tmp to user repo dir

--- toil/client.py
+++ toil/client.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from typing import Optional
+from typing import Any, Optional
 
 import requests
 
@@ -13,16 +13,24 @@ class ToilClient:
     """
 
     url: str
+    engine_parameters: dict[str, Optional[str]]
 
-    def __init__(self, host: str, port: int):
+    def __init__(
+        self, host: str, port: int, settings: Optional[dict[str, Optional[str]]] = None
+    ):
         self.url = f"{host}:{port}"
+        if settings:
+            self.engine_parameters = settings
+        else:
+            self.engine_parameters = {}
 
     def _build_url(self, suffix: str) -> str:
         return f"http://{self.url}/ga4gh/wes/v1/{suffix}"
 
-    def run_workflow(
+    def run_workflow(  # type: ignore[misc]
         self,
-        payload: RunWorkflow,
+        name: str,
+        inputs: dict[str, Any],
         attachments: Optional[list[tuple[str, BytesIO]]],
     ) -> StartRun:
         # workflow_attachment needs to be handled separately from the other parameters
@@ -31,6 +39,14 @@ class ToilClient:
             files = map(lambda a: ("workflow_attachment", a), attachments)
         else:
             files = None
+        payload = RunWorkflow(
+            workflow_url=name,
+            workflow_type="cwl",
+            # TODO: give this a real value
+            workflow_type_version="v1.2",
+            workflow_params=inputs,
+            workflow_engine_parameters=self.engine_parameters,
+        )
         resp = requests.post(
             self._build_url("runs"),
             data=payload.toil_param_format(),
