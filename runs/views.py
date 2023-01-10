f042157fcb1d3c9ed2b0eff7a086d55c6c956aba
Brian Lewis <keller@csh.rit.edu>
2023-01-10 14:11:30 +0100

    Add support for stdout and stderr endpoints from toil (#8)
    * Add support for stdout and stderr endpoints from toil
    
    * Fix imports

--- runs/views.py
+++ runs/views.py
@@ -1,16 +1,15 @@
 from typing import TYPE_CHECKING, Sequence
 
 from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.exceptions import NotFound
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.views import APIView
 
 from freva import settings
-from toil.client import RunWorkflow, ToilClient
+from toil.client import ToilClient
 from workflows.models import Workflow
 
 from .models import Run
@@ -22,14 +21,14 @@ if TYPE_CHECKING:
     )
 
 
-class RunViewSet(ViewSet):
+class RunList(APIView):
     permission_classes: Sequence["_PermissionClass"] = [IsAuthenticated]
 
-    def list(self, _request: Request) -> Response:
+    def get(self, _request: Request) -> Response:
         runs = Run.objects.all()
         return Response(RunSerializer(runs, many=True).data)
 
-    def create(self, request: Request) -> Response:
+    def post(self, request: Request) -> Response:
         toil = ToilClient(
             settings.TOIL["host"],
             settings.TOIL["port"],
@@ -63,17 +62,60 @@ class RunViewSet(ViewSet):
         run.save()
         return Response(RunSerializer(run).data)
 
-    def retrieve(self, _request: Request, pk: str) -> Response:
-        run = Run.objects.filter(id=pk).first()
+
+class RunDetail(APIView):
+    permission_classes: Sequence["_PermissionClass"] = [IsAuthenticated]
+
+    def get(self, _request: Request, run_id: str) -> Response:
+        run = Run.objects.filter(id=run_id).first()
         if run is None:
             raise NotFound
         return Response(RunSerializer(run).data)
 
-    @action(detail=True, methods=["get"])
-    def status(self, _request: Request, pk: str) -> Response:
-        toil = ToilClient(settings.TOIL["host"], settings.TOIL["port"])
-        run = Run.objects.filter(id=pk).first()
+
+class RunStatus(APIView):
+    permission_classes: Sequence["_PermissionClass"] = [IsAuthenticated]
+
+    def get(self, _request: Request, run_id: str) -> Response:
+        toil = ToilClient(
+            settings.TOIL["host"],
+            settings.TOIL["port"],
+            settings.TOIL["workflow_engine_settings"],
+        )
+        run = Run.objects.filter(id=run_id).first()
         if run is None:
             raise NotFound
         toil_info = toil.get_run_log(run.id)
         return Response(toil_info.dict())
+
+
+class RunStdout(APIView):
+    permission_classes: Sequence["_PermissionClass"] = [IsAuthenticated]
+
+    def get(self, _request: Request, run_id: str) -> Response:
+        toil = ToilClient(
+            settings.TOIL["host"],
+            settings.TOIL["port"],
+            settings.TOIL["workflow_engine_settings"],
+        )
+        run = Run.objects.filter(id=run_id).first()
+        if run is None:
+            raise NotFound
+        stdout = toil.get_stdout(run_id)
+        return Response(stdout)
+
+
+class RunStderr(APIView):
+    permission_classes: Sequence["_PermissionClass"] = [IsAuthenticated]
+
+    def get(self, _request: Request, run_id: str) -> Response:
+        toil = ToilClient(
+            settings.TOIL["host"],
+            settings.TOIL["port"],
+            settings.TOIL["workflow_engine_settings"],
+        )
+        run = Run.objects.filter(id=run_id).first()
+        if run is None:
+            raise NotFound
+        stderr = toil.get_stderr(run_id)
+        return Response(stderr)
