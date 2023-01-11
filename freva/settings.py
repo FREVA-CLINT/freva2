be75e74b314e3450b0720329266b009fd0ed1ee1
Andrej Fast <andrej.fast@dkrz.de>
2023-01-11 14:29:42 +0100

    re-added BasicAuthentication for testing-purposes.

--- freva/settings.py
+++ freva/settings.py
@@ -141,6 +141,7 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     )
 }
 
