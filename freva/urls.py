689678bc5a6b98f04e8f34a25b62d9ea915311ea
Andrej Fast <andrej.fast@dkrz.de>
2023-01-11 14:30:08 +0100

    forgot to delete dummy-endpoint from the urls...

--- freva/urls.py
+++ freva/urls.py
@@ -27,7 +27,6 @@ handler404 = views.error_404
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/getjson/", views.get_json, name="get_json"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(("workflows.urls", "workflows"), namespace="workflows")),
